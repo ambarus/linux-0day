f83ab2bf0ad592938d0e24315d1e301ba530ce77
Tudor Ambarus <tudor.ambarus@microchip.com>
2021-06-29 09:55:31 +0300

    mtd: spi-nor: Introduce Manufacturer ID collisions driver
    Some manufacturers completely ignore the manufacturer's identification code
    standard (JEP106) and do not define the manufacturer ID continuation
    scheme. This will result in manufacturer ID collisions.
    
    An an example, JEP106BA requires Boya that it's manufacturer ID to be
    preceded by 8 continuation codes. Boya's identification code must be:
    0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x68. But Boya ignores the
    continuation scheme and its ID collides with the manufacturer defined in
    bank one: Convex Computer.
    
    Introduce the manuf-id-collisions driver in order to address ID collisions
    between manufacturers. flash_info entries will be added in a first come,
    first served manner. Differentiation between flashes will be done at
    runtime if possible. Where runtime differentiation is not possible, new
    compatibles will be introduced, but this will be done as a last resort.
    Every new flash addition that define the SFDP tables, should dump its SFDP
    tables in the patch's comment section below the --- line, so that we can
    reference it in case of collisions.
    
    Signed-off-by: Tudor Ambarus <tudor.ambarus@microchip.com>

---
+++ drivers/mtd/spi-nor/manuf-id-collisions.c
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Used to handle collisions between manufacturers, where manufacturers are
+ * ignorant enough to not implement the ID continuation scheme described in the
+ * JEP106 JEDEC standard.
+ */
+
+#include <linux/mtd/spi-nor.h>
+#include "core.h"
+
+static const struct flash_info id_collision_parts[] = {
+	/* Boya */
+	{ "by25q128as", INFO(0x684018, 0, 64 * 1024, 256, SPI_NOR_SKIP_SFDP |
+			     SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ |
+			     SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB) },
+};
+
+const struct spi_nor_manufacturer spi_nor_manuf_id_collisions = {
+	.name = "manufacturer ID collisions",
+	.parts = id_collision_parts,
+	.nparts = ARRAY_SIZE(id_collision_parts),
+};
