be6846062e0bf814d6801ff5a72d71c860a05fb0
Tudor Ambarus <tudor.ambarus@microchip.com>
2021-07-26 11:19:11 +0300

    mtd: spi-nor: micron-st: n25q256a: Init flash based on SFDP
    Get rid of the static initialization of the flash parameters and
    init them when parsing SFDP.
    Generated a 256 Kbyte random data and did an erase, write, read back
    and compare test. The flash uses for reads SPINOR_OP_READ_1_4_4 0xeb,
    for erases SPINOR_OP_BE_4K 0x20, and for writes SPINOR_OP_PP 0x02.
    
    Signed-off-by: Tudor Ambarus <tudor.ambarus@microchip.com>

--- drivers/mtd/spi-nor/micron-st.c
+++ drivers/mtd/spi-nor/micron-st.c
@@ -153,9 +153,8 @@ static const struct flash_info st_parts[] = {
 			       SECT_4K | USE_FSR | SPI_NOR_DUAL_READ |
 			       SPI_NOR_QUAD_READ |
 			       FIXUP_FLAGS(SPI_NOR_4B_OPCODES)) },
-	{ "n25q256a",    INFO(0x20ba19, 0, 64 * 1024,  512, SECT_4K |
-			      USE_FSR | SPI_NOR_DUAL_READ |
-			      SPI_NOR_QUAD_READ) },
+	{ "n25q256a",    INFO(0x20ba19, 0, 64 * 1024,  512,
+			      SPI_NOR_PARSE_SFDP | NON_SFDP_FLAGS(USE_FSR)) },
 	{ "mt25qu256a",  INFO6(0x20bb19, 0x104400, 64 * 1024,  512,
 			       SECT_4K | USE_FSR | SPI_NOR_DUAL_READ |
 			       SPI_NOR_QUAD_READ |
