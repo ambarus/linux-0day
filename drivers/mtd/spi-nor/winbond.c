8aa829c784ff84bab4f851184ade79e0a6a9271a
Tudor Ambarus <tudor.ambarus@microchip.com>
2021-07-26 04:56:08 +0300

    mtd: spi-nor: winbond: w25q256jvm: Init flash based on SFDP
    Get rid of the static initialization of the flash parameters and
    init them when parsing SFDP.
    Generated a 256 Kbyte random data and did an erase, write, read back
    and compare test. The flash uses for reads SPINOR_OP_READ_1_4_4 0xeb,
    for erases SPINOR_OP_BE_4K 0x20, and for writes SPINOR_OP_PP 0x02.
    
    Signed-off-by: Tudor Ambarus <tudor.ambarus@microchip.com>

--- drivers/mtd/spi-nor/winbond.c
+++ drivers/mtd/spi-nor/winbond.c
@@ -95,7 +95,7 @@ static const struct flash_info winbond_parts[] = {
 			  SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ)
 	  .fixups = &w25q256_fixups },
 	{ "w25q256jvm", INFO(0xef7019, 0, 64 * 1024, 512,
-			     SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
+			     SPI_NOR_PARSE_SFDP) },
 	{ "w25q256jw", INFO(0xef6019, 0, 64 * 1024, 512,
 			     SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
 	{ "w25m512jv", INFO(0xef7119, 0, 64 * 1024, 1024,
