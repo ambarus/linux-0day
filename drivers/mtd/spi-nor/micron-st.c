bf9da441dce9d1c3ec8ab9f711b6a254328e9c51
Tudor Ambarus <tudor.ambarus@microchip.com>
2021-10-26 20:55:30 +0300

    mtd: spi-nor: micron-st: n25q00: Init flash based on SFDP
    Get rid of the static initialization of the flash parameters and
    init them when parsing SFDP.
    Generated a 256 Kbyte random data and did an erase, write, read back
    and compare test. The flash uses for reads SPINOR_OP_READ_1_4_4_4B 0xec,
    for erases SPINOR_OP_BE_4K_4B 0x21, and for writes SPINOR_OP_PP_1_1_4_4B
    0x34. Tested locking on the first sector.
    
    Signed-off-by: Tudor Ambarus <tudor.ambarus@microchip.com>

--- drivers/mtd/spi-nor/micron-st.c
+++ drivers/mtd/spi-nor/micron-st.c
@@ -178,10 +178,11 @@ static const struct flash_info st_parts[] = {
 			      SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB |
 			      SPI_NOR_4BIT_BP | SPI_NOR_BP3_SR_BIT6) },
 	{ "n25q00",      INFO(0x20ba21, 0, 64 * 1024, 2048,
-			      SECT_4K | USE_FSR | SPI_NOR_QUAD_READ |
-			      SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB |
-			      SPI_NOR_4BIT_BP | SPI_NOR_BP3_SR_BIT6 |
-			      NO_CHIP_ERASE) },
+			      SPI_NOR_PARSE_SFDP |
+			      NON_SFDP_FLAGS(SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB |
+					     SPI_NOR_4BIT_BP |
+					     SPI_NOR_BP3_SR_BIT6 | USE_FSR |
+					     NO_CHIP_ERASE)) },
 	{ "n25q00a",     INFO(0x20bb21, 0, 64 * 1024, 2048,
 			      SECT_4K | USE_FSR | SPI_NOR_QUAD_READ |
 			      NO_CHIP_ERASE) },
