274d29196505bde4f702c3364ace7cf8b7c8c984
Tudor Ambarus <tudor.ambarus@linaro.org>
2023-11-01 16:02:23 +0200

    atmake
    Signed-off-by: Tudor Ambarus <tudor.ambarus@linaro.org>

---
+++ atmake.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#export CROSS_COMPILE=/home/atudor/work/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabi/bin/arm-linux-gnueabi-
+#export CROSS_COMPILE=/home/atudor/work/gcc-linaro-7.3.1-2018.05-x86_64_arm-linux-gnueabi/bin/arm-linux-gnueabi-
+export CROSS_COMPILE=arm-linux-gnueabi-
+
+#make ARCH=arm COMPILED_SOURCE=1 cscope
+
+#make ARCH=arm sama7_defconfig &&
+#make ARCH=arm at91_dt_defconfig &&
+make ARCH=arm sama5_defconfig &&
+#make ARCH=arm sama5_defconfig
+#make ARCH=arm menuconfig
+#make ARCH=arm -j 16 C=1
+make ARCH=arm -j 16 W=1 &&
+make ARCH=arm dtbs
+
+#sudo mount /dev/mmcblk0p2 /mnt
+#sudo make INSTALL_MOD_PATH=/mnt modules_install -j8
+#sync
+#sudo umount /mnt
