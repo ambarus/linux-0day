bafe7f9a113167725d05a697c67c199f0be52748
Tudor Ambarus <tudor.ambarus@linaro.org>
2025-02-14 12:45:37 +0000

    firmware: samsung: add ACPM debugfs support
    The ACPM firmware saves debug information to SRAM. Add debugfs entries
    in order to expose the ACPM logs.
    
    acpm_framework/logb_gprio_level controls the ACPM print verbosity to
    the SRAM log buffer. It encodes a 64 bit value, 4 bits for each of the
    16 Plugin IDs, with verbosity levels from 0xf (log error) to
    0x0 (log debug).
    
    echo 0xffffffffffffff1f > /sys/kernel/debug/acpm_framework/logb_gprio_level
    Will will allow only LOG_ERR prints for all Plugin IDs but Plugin ID 1,
    which will issue prints for any log levels greater or equal to 1.
    On the ACPM firmware side, logb_gprio_level has a default value of zero,
    all logs enabled for all Plugin IDs.
    
    acpm_framework/log_level has a maximum value of 2 and controls which
    SRAM log buffers are printed.
    
    Finally, acpm_framework/acpm_debug_cmd provides a way to issue
    ACPM DEBUG commands to the firmware.
    
    Add ACPM debugfs support with the above capabilities.
    
    Signed-off-by: Tudor Ambarus <tudor.ambarus@linaro.org>

--- drivers/firmware/samsung/exynos-acpm.h
+++ drivers/firmware/samsung/exynos-acpm.h
@@ -19,12 +19,36 @@
  * @chans:	offset to array of struct acpm_chan_shmem.
  * @reserved1:	unused fields.
  * @num_chans:	number of channels.
+ * @reserved2:	unused fields.
+ * @log_rear:	rear pointer of APM log queue.
+ * @log_front:	front pointer of APM log queue.
+ * @log_base:	base address of APM log queue.
+ * @log_mlen:	log message length.
+ * @log_qlen:	log queue length.
+ * @reserved3:	unused fields.
+ * @preempt_log_rear:	rear pointer of APM preempt log queue.
+ * @preempt_log_front:	front pointer of APM preempt log queue.
+ * @preempt_log_base:	base address of APM preempt log queue.
+ * @preempt_log_qlen:	preempt log queue length.
+ * @reserved4:	unused fields.
  */
 struct acpm_shmem {
 	u32 reserved[2];
 	u32 chans;
 	u32 reserved1[3];
 	u32 num_chans;
+	u32 reserved2[6];
+	u32 log_rear;
+	u32 log_front;
+	u32 log_base;
+	u32 log_mlen;
+	u32 log_qlen;
+	u32 reserved3[24];
+	u32 preempt_log_rear;
+	u32 preempt_log_front;
+	u32 preempt_log_base;
+	u32 preempt_log_qlen;
+	u32 reserved4[64];
 };
 
 /**
@@ -41,23 +65,36 @@ struct acpm_queue {
 
 struct device;
 struct acpm_chan;
+struct acpm_log_info;
 
 /**
  * struct acpm_info - driver's private data.
  * @shmem:	pointer to the SRAM configuration data.
+ * @log:	pointer to the ACPM logging info.
  * @sram_base:	base address of SRAM.
  * @chans:	pointer to the ACPM channel parameters retrieved from SRAM.
  * @dev:	pointer to the exynos-acpm device.
+ * @mbox_dbg_chan: mailbox debug channel.
  * @handle:	instance of acpm_handle to send to clients.
  * @num_chans:	number of channels available for this controller.
  */
 struct acpm_info {
 	struct acpm_shmem __iomem *shmem;
+	struct acpm_log_info *log;
 	void __iomem *sram_base;
 	struct acpm_chan *chans;
 	struct device *dev;
+	unsigned int mbox_dbg_chan;
 	struct acpm_handle handle;
 	u32 num_chans;
 };
+
+#ifdef CONFIG_DEBUG_FS
+int acpm_debugfs_register(struct acpm_info *acpm);
+void acpm_debugfs_remove(void);
+#else
+static inline int acpm_debugfs_register(struct acpm_info *acpm) {}
+static inline void acpm_debugfs_remove(void) {}
+#endif
 
 #endif /* __EXYNOS_ACPM_H__ */
