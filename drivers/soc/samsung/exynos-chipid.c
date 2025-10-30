635bc88778fe6799477b2c170c749177306f0b21
Tudor Ambarus <tudor.ambarus@linaro.org>
2025-10-30 12:09:50 +0000

    soc: samsung: exynos-chipid: prepend exynos_ to a method's name
    Avoid name space pollution and prepend exynos_ to
    product_id_to_soc_id(). Add a blank line to make things readable.
    
    Signed-off-by: Tudor Ambarus <tudor.ambarus@linaro.org>

--- drivers/soc/samsung/exynos-chipid.c
+++ drivers/soc/samsung/exynos-chipid.c
@@ -70,7 +70,7 @@ static const struct exynos_soc_id {
 	{ "EXYNOSAUTOV920", 0x0A920000 },
 };
 
-static const char *product_id_to_soc_id(unsigned int product_id)
+static const char *exynos_product_id_to_soc_id(unsigned int product_id)
 {
 	int i;
 
@@ -141,7 +141,8 @@ static int exynos_chipid_probe(struct platform_device *pdev)
 						soc_info.revision);
 	if (!soc_dev_attr->revision)
 		return -ENOMEM;
-	soc_dev_attr->soc_id = product_id_to_soc_id(soc_info.product_id);
+
+	soc_dev_attr->soc_id = exynos_product_id_to_soc_id(soc_info.product_id);
 	if (!soc_dev_attr->soc_id) {
 		pr_err("Unknown SoC\n");
 		return -ENODEV;
